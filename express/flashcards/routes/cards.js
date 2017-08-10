19b199f726a89e2d03d04a3c09476480c4213343
Carlos Gomez <gomez_carlos@apple.com>
2017-08-09 17:00:27 -0700

    Updated to node version 8 for support of the latest ES6 Syntax

--- express/flashcards/routes/cards.js
+++ express/flashcards/routes/cards.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const { data } = require('../data/flashcardData.json');
+const { cards } = data;
 
 // Render Cards Route
+
+// router.get('/', (req, res) => {
+// 	res.render('card', { prompt: "Who is buried..", hint: "Think about who's tomb it is." });
+// });
+
 router.get('/', (req, res) => {
-	res.render('card', { prompt: "Who is buried..", hint: "Think about who's tomb it is." });
+	res.render('card', {
+    prompt: cards[0].question,
+    hint: cards[0].hint
+  });
 });
 
 module.exports = router;
